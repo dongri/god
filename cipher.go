db059734f7d868a7f326e54b9bf8d0bca93f711d
Dongri Jin <dongrify@gmail.com>
2016-12-10 23:02:13 +0900

    Add: cipher

---
+++ cipher.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func EncryptString(cryptKey string, text string) (string, error) {
+	encryptText, err := Encrypt(cryptKey, []byte(text))
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", encryptText), nil
+}
+
+func DecryptString(cryptKey string, text string) (string, error) {
+	b, err := hex.DecodeString(text)
+	if err != nil {
+		return "", err
+	}
+	decryptText, err := Decrypt(cryptKey, b)
+	if err != nil {
+		return "", err
+	}
+	return string(decryptText), nil
+}
+
+// Block Encrypt / Decrypt
+
+// Encrypt
+func Encrypt(cryptKey string, plainText []byte) ([]byte, error) {
+	key := []byte(cryptKey)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	encryptStream := cipher.NewCTR(block, iv)
+	encryptStream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+	// fmt.Printf("Encrypt text: %x \n", cipherText)
+	return cipherText, nil
+}
+
+// Decrypt
+func Decrypt(cryptKey string, cipherText []byte) ([]byte, error) {
+	key := []byte(cryptKey)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	decryptedText := make([]byte, len(cipherText[aes.BlockSize:]))
+	decryptStream := cipher.NewCTR(block, cipherText[:aes.BlockSize])
+	decryptStream.XORKeyStream(decryptedText, cipherText[aes.BlockSize:])
+	// fmt.Printf("Decrypted text: %s\n", string(decryptedText))
+	return decryptedText, nil
+}
---
+++ cipher_test.go
@@ -0,0 +1,20 @@
+package utils
+
+import "testing"
+
+func TestCipherString(t *testing.T) {
+	encryptedText, err := EncryptString("passwordpasswordpasswordpassword", "hoge")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	originalText, err := DecryptString("passwordpasswordpasswordpassword", encryptedText)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if originalText != "hoge" {
+		t.Errorf("got %v\nwant %v", originalText, "hoge")
+	}
+
+}
